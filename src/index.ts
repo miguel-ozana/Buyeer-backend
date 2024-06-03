ac75d28b3b23eb7537c2852675d6ab21ca43d21d
miguel-ozana <miguelozana74@gmail.com>
2024-06-03 13:19:05 -0300

    make it better

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import Express from "express";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import routes from "./router/routes";
+
+dotenv.config();
+
+const app = Express();
+const port = process.env.PORT || 8000;
+const prisma = new PrismaClient();
+
+app.use(Express.json());
+app.use("/api", routes);
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+  console.log(`http://localhost:${port}`);
+});
+  
---
+++ src/router/routes.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.get("/items", async (req, res) => {
+  const items = await prisma.item.findMany();
+  res.json(items);
+});
+
+router.post("/items", async (req, res) => {
+  const { name, quantity } = req.body;
+  const newItem = await prisma.item.create({
+    data: {
+      name,
+      quantity,
+    },
+  });
+  res.json(newItem);
+});
+
+router.put("/items/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, quantity } = req.body;
+  const updatedItem = await prisma.item.update({
+    where: { id },
+    data: { name, quantity },
+  });
+  res.json(updatedItem);
+});
+
+router.delete("/items/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.item.delete({
+    where: { id },
+  });
+  res.sendStatus(204);
+});
+
+export default router;
