d32a0f3e9ea8c59a4a82762685df1ac1f22a578e
miguel-ozana <miguelozana74@gmail.com>
2024-06-03 20:36:19 -0300

    new commit. Dotenv Config

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,11 @@ dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on http://localhost:3000');
 });
