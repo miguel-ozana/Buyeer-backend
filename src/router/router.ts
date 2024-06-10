948ec2c86cb4f1099bd3cf311f1e3add2372c791
miguel-ozana <miguelozana74@gmail.com>
2024-06-10 20:54:46 -0300

    Updated router.

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,35 +5,55 @@ const router = Router();
 const prisma = new PrismaClient();
 
 router.get('/items', async (req, res) => {
-  const items = await prisma.item.findMany();
-  res.json(items);
+  try {
+    const items = await prisma.item.findMany();
+    res.json(items);
+  } catch (error) {
+    console.error('Erro ao buscar os itens:', error);
+    res.status(500).json({ error: 'Erro ao buscar os itens' });
+  }
 });
 
 router.post('/items', async (req, res) => {
   const { name, quantity } = req.body;
-  const newItem = await prisma.item.create({
-    data: {
-      name,
-      quantity,
-    },
-  });
-  res.json(newItem);
+  try {
+    const newItem = await prisma.item.create({
+      data: {
+        name,
+        quantity,
+      },
+    });
+    res.status(201).json(newItem);
+  } catch (error) {
+    console.error('Erro ao criar um novo item:', error);
+    res.status(500).json({ error: 'Erro ao criar um novo item' });
+  }
 });
 
 router.put('/items/:id', async (req, res) => {
   const { id } = req.params;
   const { name, quantity } = req.body;
-  const updatedItem = await prisma.item.update({
-    where: { id: id },
-    data: { name, quantity },
-  });
-  res.json(updatedItem);
+  try {
+    const updatedItem = await prisma.item.update({
+      where: { id: id.toString() }, // Convertendo para string
+      data: { name, quantity },
+    });
+    res.json(updatedItem);
+  } catch (error) {
+    console.error('Erro ao atualizar o item:', error);
+    res.status(500).json({ error: 'Erro ao atualizar o item' });
+  }
 });
 
 router.delete('/items/:id', async (req, res) => {
   const { id } = req.params;
-  await prisma.item.delete({ where: { id: id } });
-  res.sendStatus(204);
+  try {
+    await prisma.item.delete({ where: { id: id.toString() } }); // Convertendo para string
+    res.sendStatus(204);
+  } catch (error) {
+    console.error('Erro ao deletar o item:', error);
+    res.status(500).json({ error: 'Erro ao deletar o item' });
+  }
 });
 
 
