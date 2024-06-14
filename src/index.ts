748e4682ab20913cd64de43e3a9cbb79bc5eef5d
miguel-ozana <miguelozana74@gmail.com>
2024-06-13 23:07:00 -0300

    add bought to database

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
 const corsOptions = { 
-  origin: 'http://localhost:3000',
+  origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin', 'Accept', 'X-Access-Token']
