147f962bbc558e3fe929bddfd1f0716214b7e5d8
miguel-ozana <miguelozana74@gmail.com>
2024-06-14 23:53:03 -0300

    created prismamiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import routes from './router/router';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 dotenv.config();
@@ -10,11 +10,20 @@ const app = express();
 const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
-const corsOptions = { 
-  origin: "https://buyeer.vercel.app",
+const corsOptions = {
+  origin: 'https://buyeer.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin', 'Accept', 'X-Access-Token']
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'Origin',
+    'Accept',
+    'X-Access-Token',
+    'x-device-id', // Adicione x-device-id aos cabeçalhos permitidos
+  ],
+  exposedHeaders: ['x-device-id'], // Exponha x-device-id para que o navegador possa acessá-lo
 };
 
 app.use(cors(corsOptions));
